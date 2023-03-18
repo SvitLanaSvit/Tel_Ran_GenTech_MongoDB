9f7901aeec2cf20dc9bc21e0ab1cc2880cc5343d
Spil_jf00@student.itstep.org <Spil_jf00@student.itstep.org>
2023-03-18 13:20:58 +0100

    added DZ_4

---
+++ DZ_4.js
@@ -0,0 +1,56 @@
+//(1) Вывести количественное распределение товаров по поставщикам, а также общую сумму поставленных товаров
+db.goods.aggregate([
+    {
+        '$group': {
+            '_id': '$supplier_id',
+            'total_sum_product': {
+                '$sum': '$quantity'
+            },
+            'total_price_products': {
+                '$sum': {
+                    '$multiply': ['$quantity', '$price']
+                }
+            }
+        }
+    }
+])
+
+//(2) Вывести общую и среднюю продолжительность звонков по каждой теме
+db.calls.aggregate([
+    {
+        '$group': {
+            '_id': '$topic',
+            'total_duration': {'$sum': '$duration_secs'},
+            'avg_duration': {'$avg': '$duration_secs'}
+        }
+    }
+])
+
+//(3) Вывести тему звонков, по которой общались меньше всего
+db.calls.aggregate([
+    {
+        '$group': {
+            '_id': '$topic',
+            'total_duration': {'$sum': '$duration_secs'}
+        }
+    },
+    {
+        '$sort': {
+            'total_duration': 1
+        }
+    },
+    {
+        '$limit': 1
+    },
+    
+    {
+        '$project': {
+            'theme_of_call': '$_id',
+            '_id': 0,
+            'total_duration': 1
+        }
+    },
+    {
+        '$unwind': '$theme_of_call'
+    }
+])
